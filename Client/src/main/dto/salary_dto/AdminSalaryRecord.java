ad0dfff7e7de0f5d9e5ad911fbc2fafebe2c57b2
이경훈 <100902332+lgh000322@users.noreply.github.com>
2024-09-13 23:07:00 +0900

    Feat: 관리자 탭 기능수정

---
+++ Client/src/main/dto/salary_dto/AdminSalaryRecord.java
@@ -0,0 +1,105 @@
+package main.dto.salary_dto;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleLongProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+public class AdminSalaryRecord {
+	
+	private final SimpleLongProperty salaryNum;
+    private final SimpleStringProperty salaryName;
+    private final SimpleStringProperty salaryPhone;
+    private final SimpleStringProperty salaryDept;
+    private final SimpleStringProperty salaryPosition;
+    private final SimpleIntegerProperty salaryPayment;
+
+    // 생성자
+    public AdminSalaryRecord(Long salaryNum, String salaryName, String salaryPhone, String salaryDept, 
+                             String salaryPosition, Integer salaryPayment) {
+        this.salaryNum = new SimpleLongProperty(salaryNum);
+        this.salaryName = new SimpleStringProperty(salaryName);
+        this.salaryPhone = new SimpleStringProperty(salaryPhone);
+        this.salaryDept = new SimpleStringProperty(salaryDept);
+        this.salaryPosition = new SimpleStringProperty(salaryPosition);
+        this.salaryPayment = new SimpleIntegerProperty(salaryPayment);
+    }
+
+    // Property 반환 메서드
+    public LongProperty salaryNumProperty() {
+        return salaryNum;
+    }
+
+    public StringProperty salaryNameProperty() {
+        return salaryName;
+    }
+
+    public StringProperty salaryPhoneProperty() {
+        return salaryPhone;
+    }
+
+    public StringProperty salaryDeptProperty() {
+        return salaryDept;
+    }
+
+    public StringProperty salaryPositionProperty() {
+        return salaryPosition;
+    }
+
+    public IntegerProperty salaryPaymentProperty() {
+        return salaryPayment;
+    }
+
+    // Getter methods
+    public long getSalaryNum() {
+        return salaryNum.get();
+    }
+
+    public void setSalaryNum(long salaryNum) {
+        this.salaryNum.set(salaryNum);
+    }
+
+    public String getSalaryName() {
+        return salaryName.get();
+    }
+
+    public void setSalaryName(String salaryName) {
+        this.salaryName.set(salaryName);
+    }
+
+    public String getSalaryPhone() {
+        return salaryPhone.get();
+    }
+
+    public void setSalaryPhone(String salaryPhone) {
+        this.salaryPhone.set(salaryPhone);
+    }
+
+    public String getSalaryDept() {
+        return salaryDept.get();
+    }
+
+    public void setSalaryDept(String salaryDept) {
+        this.salaryDept.set(salaryDept);
+    }
+
+    public String getSalaryPosition() {
+        return salaryPosition.get();
+    }
+
+    public void setSalaryPosition(String salaryPosition) {
+        this.salaryPosition.set(salaryPosition);
+    }
+
+    public int getSalaryPayment() {
+        return salaryPayment.get();
+    }
+
+    public void setSalaryPayment(int salaryPayment) {
+        this.salaryPayment.set(salaryPayment);
+    }
+}
+
+
--- Client/src/main/dto/user_dto/UserRecord.java
+++ Client/src/main/dto/user_dto/UserRecord.java
@@ -1,54 +1,99 @@
 package main.dto.user_dto;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-
 import javafx.beans.property.SimpleLongProperty;
 import javafx.beans.property.SimpleStringProperty;
-import main.domain.work_log.Status;
+import javafx.beans.property.LongProperty;
+import javafx.beans.property.StringProperty;
 
 public class UserRecord {
-	private final SimpleLongProperty num;
-	private final SimpleStringProperty name;
-	private final SimpleStringProperty tel;
-	private final SimpleStringProperty dept;
-	private final SimpleStringProperty position;
-	private final SimpleStringProperty email;
+    private final SimpleLongProperty num;
+    private final SimpleStringProperty name;
+    private final SimpleStringProperty tel;
+    private final SimpleStringProperty dept;
+    private final SimpleStringProperty position;
+    private final SimpleStringProperty email;
 
-	public UserRecord(Long num, String name, String tel,
-			String dept, String position, String email) {
-		super();
-		this.num = new SimpleLongProperty(num);
-		this.name = new SimpleStringProperty(name);
-		this.tel = new SimpleStringProperty(tel);
-		this.dept = new SimpleStringProperty(dept);
-		this.position = new SimpleStringProperty(position);
-		this.email = new SimpleStringProperty(email);
-		
-	}
+    // 생성자
+    public UserRecord(Long num, String name, String tel, String dept, String position, String email) {
+        this.num = new SimpleLongProperty(num);
+        this.name = new SimpleStringProperty(name);
+        this.tel = new SimpleStringProperty(tel);
+        this.dept = new SimpleStringProperty(dept);
+        this.position = new SimpleStringProperty(position);
+        this.email = new SimpleStringProperty(email);
+    }
 
-	public SimpleLongProperty getNum() {
-		return num;
-	}
+    // Property 반환 메서드
+    public LongProperty numProperty() {
+        return num;
+    }
 
-	public SimpleStringProperty getName() {
-		return name;
-	}
+    public StringProperty nameProperty() {
+        return name;
+    }
 
-	public SimpleStringProperty getTel() {
-		return tel;
-	}
+    public StringProperty telProperty() {
+        return tel;
+    }
 
-	public SimpleStringProperty getDept() {
-		return dept;
-	}
+    public StringProperty deptProperty() {
+        return dept;
+    }
 
-	public SimpleStringProperty getPosition() {
-		return position;
-	}
+    public StringProperty positionProperty() {
+        return position;
+    }
 
-	public SimpleStringProperty getEmail() {
-		return email;
-	}
+    public StringProperty emailProperty() {
+        return email;
+    }
 
+    // Getter methods
+    public long getNum() {
+        return num.get();
+    }
+
+    public void setNum(long num) {
+        this.num.set(num);
+    }
+
+    public String getName() {
+        return name.get();
+    }
+
+    public void setName(String name) {
+        this.name.set(name);
+    }
+
+    public String getTel() {
+        return tel.get();
+    }
+
+    public void setTel(String tel) {
+        this.tel.set(tel);
+    }
+
+    public String getDept() {
+        return dept.get();
+    }
+
+    public void setDept(String dept) {
+        this.dept.set(dept);
+    }
+
+    public String getPosition() {
+        return position.get();
+    }
+
+    public void setPosition(String position) {
+        this.position.set(position);
+    }
+
+    public String getEmail() {
+        return email.get();
+    }
+
+    public void setEmail(String email) {
+        this.email.set(email);
+    }
 }
