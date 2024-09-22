8026507a3929a6a4771606870a8dd96a54ef9a82
이경훈 <100902332+lgh000322@users.noreply.github.com>
2024-09-23 01:49:49 +0900

    Delete DataBase_sql/StringToEnum.sql

--- DataBase_sql/StringToEnum.sql
+++
@@ -1,3 +0,0 @@
-alter table user modify column role ENUM('ADMIN','USER');
-alter table mail_store modify column mail_type ENUM('RECEIVED','SEND');
-alter table work_log modify column status ENUM('ABSENCE','TARDINESS','ATTENDANCE','LEAVE');
