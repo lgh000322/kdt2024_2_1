185695c9d27e836970a90bb8e952bff48edb8c24
이경훈 <100902332+lgh000322@users.noreply.github.com>
2024-09-22 18:13:07 +0900

    Feat: 초기화 파일 수정

--- DataBase_sql/초기화 sql파일(프로젝트 초기설정).sql
+++ DataBase_sql/초기화 sql파일(프로젝트 초기설정).sql
@@ -163,6 +163,7 @@ CREATE TABLE IF NOT EXISTS `erp_db`.`received_mail` (
   `received_num` BIGINT NOT NULL AUTO_INCREMENT,
   `user_num` BIGINT NOT NULL,
   `mail_num` BIGINT NOT NULL,
+  is_deleted TINYINT(1) DEFAULT 0,
   PRIMARY KEY (`received_num`),
   UNIQUE INDEX `received_num_UNIQUE` (`received_num` ASC),
   INDEX `fk_received_mail_user1_idx` (`user_num` ASC),
@@ -268,8 +269,130 @@ INSERT INTO dept (dept_name) VALUES
 ('법무부');
 
 INSERT INTO `user` (user_id, password, name, tel, email, role, remained_leave, position_num, dept_num)
-VALUES ('admin', 'admin', 'admin', '010-1111-1111', 'admin@knu.com', 'ADMIN', 21, 1, 1);
+VALUES ('admin', 'admin', 'admin', '010-1111-1111', 'admin@knu.com', 'ADMIN', 21, 7, 2);
 
-insert into mail_store (mail_type,user_num) values('SEND',1);
-insert into mail_store (mail_type,user_num) values('RECEIVED',1);
+INSERT INTO `user` (user_id, password, name, tel, email, role, remained_leave, position_num, dept_num)
+VALUES 
+('normaluser1', 'normaluserpassword1', '홍길동', '010-2222-2222', 'normaluser1@naver.com', 'USER', 15, 1, 1),
+('normaluser2', 'normaluserpassword2', '김철수', '010-3333-3333', 'normaluser2@naver.com', 'USER', 15, 1, 1),
+('normaluser3', 'normaluserpassword3', '이영희', '010-4444-4444', 'normaluser3@naver.com', 'USER', 15, 1, 1),
+('normaluser4', 'normaluserpassword4', '박민수', '010-5555-5555', 'normaluser4@naver.com', 'USER', 15, 1, 1),
+('normaluser5', 'normaluserpassword5', '최지은', '010-6666-6666', 'normaluser5@naver.com', 'USER', 15, 1, 1),
+('normaluser6', 'normaluserpassword6', '이재영', '010-7777-7777', 'normaluser6@naver.com', 'USER', 15, 1, 1),
+('normaluser7', 'normaluserpassword7', '정하늘', '010-8888-8888', 'normaluser7@naver.com', 'USER', 15, 1, 1),
+('normaluser8', 'normaluserpassword8', '김소연', '010-9999-9999', 'normaluser8@naver.com', 'USER', 15, 1, 1),
+('normaluser9', 'normaluserpassword9', '이수민', '010-1010-1010', 'normaluser9@naver.com', 'USER', 15, 1, 1),
+('normaluser10', 'normaluserpassword10', '박지민', '010-2020-2020', 'normaluser10@naver.com', 'USER', 15, 1, 1),
+('normaluser11', 'normaluserpassword11', '최성훈', '010-3030-3030', 'normaluser11@naver.com', 'USER', 15, 1, 1),
+('normaluser12', 'normaluserpassword12', '양하나', '010-4040-4040', 'normaluser12@naver.com', 'USER', 15, 1, 1),
+('normaluser13', 'normaluserpassword13', '이재훈', '010-5050-5050', 'normaluser13@naver.com', 'USER', 15, 1, 1),
+('normaluser14', 'normaluserpassword14', '정민지', '010-6060-6060', 'normaluser14@naver.com', 'USER', 15, 1, 1),
+('normaluser15', 'normaluserpassword15', '이동준', '010-7070-7070', 'normaluser15@naver.com', 'USER', 15, 1, 1),
+('normaluser16', 'normaluserpassword16', '김태현', '010-8080-8080', 'normaluser16@naver.com', 'USER', 15, 1, 1),
+('normaluser17', 'normaluserpassword17', '정지우', '010-9090-9090', 'normaluser17@naver.com', 'USER', 15, 1, 1),
+('normaluser18', 'normaluserpassword18', '최유리', '010-1111-2222', 'normaluser18@naver.com', 'USER', 15, 1, 1),
+('normaluser19', 'normaluserpassword19', '이승현', '010-2222-3333', 'normaluser19@naver.com', 'USER', 15, 1, 1),
+('normaluser20', 'normaluserpassword20', '박상혁', '010-3333-4444', 'normaluser20@naver.com', 'USER', 15, 1, 1);
+
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 1);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 1);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 2);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 2);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 3);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 3);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 4);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 4);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 5);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 5);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 6);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 6);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 7);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 7);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 8);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 8);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 9);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 9);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 10);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 10);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 11);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 11);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 12);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 12);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 13);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 13);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 14);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 14);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 15);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 15);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 16);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 16);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 17);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 17);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 18);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 18);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 19);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 19);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 20);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 20);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('SEND', 21);
+INSERT INTO mail_store (mail_type, user_num) VALUES ('RECEIVED', 21);
+
+-- User 1의 salary_log 삽입
+INSERT INTO salary_log (received_date, total_salary, user_num) VALUES ('2024-03-10', 6000000, 1);
+INSERT INTO salary_log (received_date, total_salary, user_num) VALUES ('2024-04-10', 6000000, 1);
+INSERT INTO salary_log (received_date, total_salary, user_num) VALUES ('2024-05-10', 6000000, 1);
+INSERT INTO salary_log (received_date, total_salary, user_num) VALUES ('2024-06-10', 6000000, 1);
+INSERT INTO salary_log (received_date, total_salary, user_num) VALUES ('2024-07-10', 6000000, 1);
+INSERT INTO salary_log (received_date, total_salary, user_num) VALUES ('2024-08-10', 6000000, 1);
+INSERT INTO salary_log (received_date, total_salary, user_num) VALUES ('2024-09-10', 6000000, 1);
+
+DROP PROCEDURE IF EXISTS my_procedure;
+DROP PROCEDURE IF EXISTS insert_work_log;
+
+-- User 2부터 20까지의 salary_log 삽입
+DELIMITER //
+
+CREATE PROCEDURE my_procedure()
+BEGIN
+    DECLARE i INT DEFAULT 2;
+    
+    WHILE i <= 21 DO
+        INSERT INTO salary_log (received_date, total_salary, user_num) VALUES ('2024-03-10', 3000000, i);
+        INSERT INTO salary_log (received_date, total_salary, user_num) VALUES ('2024-04-10', 3000000, i);
+        INSERT INTO salary_log (received_date, total_salary, user_num) VALUES ('2024-05-10', 3000000, i);
+        INSERT INTO salary_log (received_date, total_salary, user_num) VALUES ('2024-06-10', 3000000, i);
+        INSERT INTO salary_log (received_date, total_salary, user_num) VALUES ('2024-07-10', 3000000, i);
+        INSERT INTO salary_log (received_date, total_salary, user_num) VALUES ('2024-08-10', 3000000, i);
+        INSERT INTO salary_log (received_date, total_salary, user_num) VALUES ('2024-09-10', 3000000, i);
+        
+        SET i = i + 1;
+    END WHILE;
+END //
+
+DELIMITER ;
+
+DELIMITER //
+
+CREATE PROCEDURE insert_work_log()
+BEGIN
+    DECLARE i INT;
+    DECLARE j INT;
+
+    SET j = 0;
+    WHILE j < 5 DO
+        SET j = j + 1;
+        SET i = 1;
+        
+        WHILE i <= 21 DO
+            INSERT INTO work_log (start_time, end_time, status, work_date, user_num) 
+            VALUES ('08:50', '17:52', 'ATTENDANCE', DATE_ADD('2024-09-16', INTERVAL j DAY), i);
+            SET i = i + 1;
+        END WHILE;
+    END WHILE;
+END //
+
+DELIMITER ;
+
+CALL my_procedure();
+CALL insert_work_log();
 
